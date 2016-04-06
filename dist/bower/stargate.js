83c960429f6e79835026c202f381e8c80ad58ebe
pasquale <pasquale.mangialavori@gmail.com>
2016-04-06 18:10:08 +0200

    Dist with last fix

--- dist/bower/stargate.js
+++ dist/bower/stargate.js
@@ -1245,7 +1245,9 @@
         }
 
         for(var j = 0; j < toRemove.length;j++){
-            toRemove[j].parentNode.removeChild(toRemove[j]);
+            if(toRemove[j]){
+                toRemove[j].parentNode.removeChild(toRemove[j]);
+            }
         }
 
         return dom;
