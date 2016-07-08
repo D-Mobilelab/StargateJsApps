f57dee186a53f8717d097e940c48e3983a6d21ee
Mirco Cipriani <mirco.cipriani@buongiorno.com>
2016-07-08 11:47:33 +0200

    dist feature-iaplight

--- dist/stargate.js
+++ dist/stargate.js
@@ -6266,82 +6266,6 @@ window.pubKey = '';
 window.forge = '';
 
 
-var initOfflinePromise;
-
-/**
- * Initialize offline will be resolved at the deviceready event or rejected after a timeout
- * @param {object} [options={}] - an object with offline initialization options
- * @param [options.hideSplashScreen=true] - a boolean indicating to hide or not the splash screen
- * @returns {Promise<boolean>}
- * 
- * @deprecated since v0.2.8
- * */
-stargatePublic.initializeOffline = function(options){
-
-    if(initOfflinePromise) {
-        return initOfflinePromise;
-    }
-    
-    // - start set default options -
-    if (typeof options !== "object") {
-        options = {};
-    }
-    if (! options.hasOwnProperty("hideSplashScreen")) {
-        options.hideSplashScreen = true;
-    }
-    // -- end set default options --
-    
-    isStargateInitialized = true;
-    initOfflinePromise = new Promise(function (initOfflineResolve) {
-        document.addEventListener("deviceready", function deviceReadyOffline() {
-
-            // device ready received so i'm sure to be hybrid
-            setIsHybrid();
-            
-            // get device information
-            initDevice();
-            
-            // get connection information
-            initializeConnectionStatus();
-
-            // request all asyncronous initialization to complete
-            Promise.all([
-                // include here all needed asyncronous initializazion
-                cordova.getAppVersion.getVersionNumber(),
-                getManifest()
-            ])
-            .then(function(results) {
-                // save async initialization result
-
-                appVersion = results[0];
-                
-                if (typeof results[1] !== 'object') {
-                    results[1] = JSON.parse(results[1]);
-                }
-
-                stargateConf = results[1].stargateConf;
-                
-                if (options.hideSplashScreen) {
-                    navigator.splashscreen.hide();
-                    setBusy(false);                    
-                }
-
-                // initialize finished
-                isStargateOpen = true;
-
-                log("Stargate.initializeOffline() done");
-
-                initOfflineResolve(true);
-
-            })
-            .catch(function (error) {
-                err("initializeOffline() error: "+error);
-            });
-        });
-    });
-    return initOfflinePromise;
-};
-
 
 /**
  * Stargate application configuration getters namespace
@@ -6875,6 +6799,27 @@ var appPackageName = '';
  */
 var appIsDebug = false;
 
+/**
+ * InApp Purchase module type:
+ *  1 => use cordova plugin https://github.com/j3k0/cordova-plugin-purchase
+ *  2 => use cordova plugin https://github.com/AlexDisler/cordova-plugin-inapppurchase
+ */
+var stargateIapType = 0;
+
+var STARGATEIAPTYPES = {
+    /**
+     * use cordova plugin https://github.com/j3k0/cordova-plugin-purchase
+     */
+    "full": 1,
+
+    /**
+     * use cordova plugin https://github.com/AlexDisler/cordova-plugin-inapppurchase
+     */
+    "light": 2,
+    
+    1: "full",
+    2: "light"
+};
 
 /**
  * 
@@ -7011,7 +6956,8 @@ var onPluginReady = function (resolve) {
         IAP.initialize(
             getModuleConf("iapbase")
         );
-        
+        stargateIapType = STARGATEIAPTYPES.full;
+
     } else if (haveRequestedFeature("iap")) {
         // if initialize ok...
         if ( IAP.initialize( getModuleConf("iap") ) ) {
@@ -7020,6 +6966,15 @@ var onPluginReady = function (resolve) {
             //IAP.doRefresh();
             log("Init IAP done.");
         }
+        stargateIapType = STARGATEIAPTYPES.full;
+        
+    } else if (haveRequestedFeature("iaplight")) {
+        // iap new implementation
+        // (we don't need to wait for promis to fullfill)
+        iaplight.initialize(
+            getModuleConf("iaplight")
+        );
+        stargateIapType = STARGATEIAPTYPES.light;
     }
 
     // receive appsflyer conversion data event
@@ -7048,7 +7003,7 @@ var onPluginReady = function (resolve) {
             )
         );
     }
-        
+
     if (haveRequestedFeature("adv") && stargateModules.AdManager) {
         // save initialization promise, to wait for
         modulePromises.push(
@@ -8245,12 +8200,266 @@ var iaplight = (function(){
     
 	var protectedInterface = {};
 
-    protectedInterface.initialize = function() {
-        return true;
+    /*
+     * inAppPurchase.getProducts(["com.mycompany.myproduct.weekly.v1"]).then(function(res){console.log("res:"+JSON.stringify(res))})
+     * res:[
+     *     {
+     *         "productId": "com.mycompany.myproduct.weekly.v1",
+     *         "title": "Abbonamento Premium CalcioStar Italia",
+     *         "description": "Abonamento premium al catalogo CalcioStar Italia",
+     *         "price": "€0,99"
+     *     }
+     * ]
+     * 
+     * inAppPurchase.subscribe("com.mycompany.myproduct.weekly.v1").then(function(res){console.log("res:"+JSON.stringify(res))}).catch(function(err){console.error(err)})
+     * res:{
+     *     "transactionId":"1000000221696692",
+     *     "receipt":"MXXXX"
+     * }
+     * 
+     * inAppPurchase.getReceiptBundle().then(function(res){console.log("res:"+JSON.stringify(res))})
+     * res:{
+     *     "originalAppVersion": "1.0",
+     *     "appVersion": "0.1.0",
+     *     "inAppPurchases": [
+     *         {
+     *             "transactionIdentifier":"1000000221696692",
+     *             "quantity":1,
+     *             "purchaseDate":"2016-07-05T10:15:21Z",
+     *             "productId":"com.mycompany.myproduct.weekly.v1",
+     *             "originalPurchaseDate":"2016-07-05T10:15:22Z",
+     *             "subscriptionExpirationDate":"2016-07-05T10:18:21Z",
+     *             "originalTransactionIdentifier":"1000000221696692",
+     *             "webOrderLineItemID":-1497665198,
+     *             "cancellationDate":null}
+     *     ],
+     *     "bundleIdentifier": "com.mycompany.myproduct"
+     * }
+    */
+
+    /**
+     * 
+     * Initialization promise generated from window.inAppPurchase.getProducts
+     * all public interface wait for this promise to resolve
+     * 
+     */
+    var initPromise = null;
+    
+
+    /**
+     * Array of in app products id requested by webapp
+     */
+    var productsId = [];
+
+    /**
+     * Array of in app product information, like this: 
+     * [{
+     *   "productId": "com.mycompany.myproduct.weekly.v1",
+     *   "title": "Premium subscription to myproduct",
+     *   "description": "Premium subscription to my beatiful product",
+     *   "price": "€0,99"
+     * }]
+     */
+    var productsInfo = [];
+
+    /**
+     * @param {object} initializeConf - configuration sent by
+     * @return {boolean} - true if init ok
+     */
+	protectedInterface.initialize = function(initializeConf) {
+
+        if (!window.inAppPurchase) {
+            return Promise.reject("inAppPurchase not available, missing cordova plugin.");
+        }
+
+        if (initPromise !== null) {
+            return initPromise;
+        }
+
+        if (initializeConf.productsIdAndroid || initializeConf.productsIdIos) {
+            if (isRunningOnAndroid()) {
+                productsId = initializeConf.productsIdAndroid;
+            }
+            else if (isRunningOnIos()) {
+                productsId = initializeConf.productsIdIos;
+            }
+        }
+
+        if (!productsId) {
+            return Promise.reject("missing parameter productsId(Android|Ios)");
+        }
+        if (productsId && productsId.constructor !== Array) {
+            return Promise.reject("parameter error, productsId(Android|Ios) must be an array");
+        }
+        if (productsId.length === 0) {
+            return Promise.reject("parameter error, productsId(Android|Ios) must contains at least a productid");
+        }
+        
+
+        initPromise = window.inAppPurchase.getProducts(productsId)
+            .then(function(res){
+                productsInfo = res;
+                log("[IAPlight] getProducts ok", res);
+                return res;
+            })
+            .catch(function(error) {
+                err("[IAPlight] getProducts KO", error);
+            });
+
+        return initPromise;
+    };
+
+    protectedInterface.subscribe = function(productId) {
+        
+        if (initPromise === null) {
+            return Promise.reject("Not initialized");
+        }
+
+        var subFunc = function() {
+            return window.inAppPurchase.subscribe(
+                productId
+            )
+            .then(function(res){
+                log("[IAPlight] subscribe ok", res);
+                return res;
+            })
+            .catch(function(error){
+                err("[IAPlight] subscribe KO: "+error, error);
+                //throw err;
+            });
+        };
+
+        // wait for initPromise if it didn't complete
+        return initPromise.then(subFunc);
+    };
+
+    protectedInterface.getExpireDate = function(productId) {
+        
+        if (initPromise === null) {
+            return Promise.reject("Not initialized");
+        }
+
+        var receiptFunc = function() {
+            return window.inAppPurchase.getReceiptBundle()
+            .then(function(res){
+                // return last purchase receipt (ordered by last subscriptionExpirationDate)
+
+                log("[IAPlight] getExpireDate getReceiptBundle ok", res);
+
+                /* res:{ "originalAppVersion": "1.0",
+                *        "appVersion": "0.1.0",
+                *        "inAppPurchases": [ {
+                *                "transactionIdentifier":"123412341234",
+                *                "quantity":1,
+                *                "purchaseDate":"2016-07-05T10:15:21Z",
+                *                "productId":"com.mycompany.myapp.weekly.v1",
+                *                "originalPurchaseDate":"2016-07-05T10:15:22Z",
+                *                "subscriptionExpirationDate":"2016-07-05T10:18:21Z",
+                *                "originalTransactionIdentifier":"123412341234",
+                *                "webOrderLineItemID":-1497665198,
+                *                "cancellationDate":null}
+                *        ],
+                *        "bundleIdentifier": "com.mycompany.myapp" }
+                */
+                var lastPurchase = {};
+                if (res.inAppPurchases && res.inAppPurchases.constructor === Array) {
+                    res.inAppPurchases.forEach(function(inAppPurchase) {
+                        
+                        // filter out other productIds
+                        if (inAppPurchase.productId == productId) {
+
+                            // if 
+                            if (!lastPurchase.subscriptionExpirationDate) {
+                                lastPurchase = inAppPurchase;
+                                return;
+                            }
+
+                            var lastExp = new Date(lastPurchase.subscriptionExpirationDate);
+                            var currExp = new Date(inAppPurchase.subscriptionExpirationDate);
+
+                            if (lastExp < currExp) {
+                                lastPurchase = inAppPurchase;
+                                return;
+                            }
+                        }
+                    });
+                }
+
+                return lastPurchase;
+            })
+            .then(function(lastPurchase) {
+                // return expiration date
+
+                log("[IAPlight] getExpireDate lastPurchase ok", lastPurchase);
+
+                return new Date(lastPurchase.subscriptionExpirationDate);
+            })
+            .catch(function(error){
+                err("[IAPlight] getReceiptBundle KO: "+error, error);
+                //throw err;
+            });
+        };
+
+        // wait for initPromise if it didn't complete
+        return initPromise.then(receiptFunc);
+    };
+
+    protectedInterface.getProductInfo = function(productId) {
+        
+        if (initPromise === null) {
+            return Promise.reject("Not initialized");
+        }
+
+        // wait for initPromise if it didn't complete
+        return initPromise.then(function(){
+
+            var rightProductInfo = {};
+
+            productsInfo.forEach(function(productInfo) {
+                if (productInfo.productId == productId) {
+                    rightProductInfo = productInfo;
+                }
+            });
+            log("[IAPlight] getProductInfo(): product found:", rightProductInfo);
+
+            return rightProductInfo;
+        });
+    };
+
+    /**
+     * 
+     * Check that stargate is properly initialized befor calling the function innerMethod
+     */
+    var checkDecorator = function(innerMethod) {
+        return function () {
+
+            if (!isStargateInitialized) {
+                return Promise.reject("Stargate not initialized, call Stargate.initialize first!");
+            }
+            if (!isStargateOpen) {
+                return Promise.reject("Stargate closed, wait for Stargate.initialize to complete!");
+            }
+
+            return innerMethod.apply(null, arguments);
+        };
+    };
+
+    protectedInterface.public = {
+        "initialize": checkDecorator(protectedInterface.initialize),
+        "getProductInfo": checkDecorator(protectedInterface.getProductInfo),
+        "subscribe": checkDecorator(protectedInterface.subscribe),
+        "getExpireDate": checkDecorator(protectedInterface.getExpireDate)
+    };
+
+    protectedInterface.__clean__ = function() {
+        initPromise = null;
     };
 
     return protectedInterface;
 })();
+
+stargatePublic.iaplight = iaplight.public;
+
 
 var IAP = {
 
@@ -8662,7 +8871,9 @@ var IAP = {
         // start first attempt
         createUserAjaxCall();
         
-	}
+	},
+
+    
 };
 
 
