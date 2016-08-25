4c555258c7c4b0b845563bdfc4f1828b90dd782c
Mirco Cipriani <mirco.cipriani@buongiorno.com>
2016-08-25 11:53:54 +0200

    0.7.4

--- dist/bower/stargate.js
+++ dist/bower/stargate.js
@@ -18,7 +18,7 @@
     }
 }(this, function () {
     // Public interface
-    var stargatePackageVersion = "0.7.3";
+    var stargatePackageVersion = "0.7.4";
     var stargatePublic = {};
     
     var stargateModules = {};       
