f3dc1a7d7dcf66b4a229c854aaba59d9e264114e
Mirco Cipriani <mirco.cipriani@docomodigital.com>
2017-07-12 08:43:35 +0200

    new get manifest method

--- src/stargate.js
+++ src/stargate.js
@@ -144,10 +144,6 @@ function getManifestWithCordovaFilePlugin() {
 
 function getManifest() {
     
-    if (window.cordova.file) {
-        return getManifestWithCordovaFilePlugin();
-    }
-    
     if (window.hostedwebapp) {
         return new Promise(function(resolve,reject){
             window.hostedwebapp.getManifest(
@@ -160,6 +156,10 @@ function getManifest() {
                 }
             );
         });
+    }
+
+    if (window.cordova.file) {
+        return getManifestWithCordovaFilePlugin();
     }
     
     err("getManifest() no available reading mechanism!");
